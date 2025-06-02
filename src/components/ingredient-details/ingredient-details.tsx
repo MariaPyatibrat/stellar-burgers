e05552d6dba20f8578311d2af5ce3e5e29d40a44
Maria Pyatibrat <masapyatibrat@gmail.com>
2025-06-02 19:24:57 +0300

    - добавление ингредиентов в бургер

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react';
+import { useParams } from 'react-router-dom';
+import { useAppSelector } from '../../services/store';
+import { selectIngredients } from '../../services/slice/ingredientsSlice';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  const { id } = useParams();
+  const ingredients = useAppSelector(selectIngredients);
+  const ingredientData = ingredients.find((ing) => ing._id === id);
 
   if (!ingredientData) {
     return <Preloader />;
